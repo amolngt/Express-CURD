84667b5cd27a6d9ef98f472dcaf4708b4e9588d0
Amol <amol.nagotkar@gmail.com>
2020-01-05 10:27:49 +0530

    register message added

--- controller/auth.js
+++ controller/auth.js
@@ -4,7 +4,7 @@ const authUtils = require('../utils/auth');
 const passport = require('passport');
 const usermodel= require('../model/users');
 const flash = require('connect-flash');
-router.use(flash())
+
 router.get('/login', (req, res, next) => {
   const messages = req.flash();
 
@@ -15,12 +15,15 @@ router.post('/login', passport.authenticate('local',
       res.redirect('/users');
 });
 router.get('/register', (req, res, next) => {
-  // const messages = req.flash();
-  res.render('employee/register');
+  const messages = req.flash();
+  res.render('employee/register',{ messages });
 });
 router.post('/register', (req, res, next) => {
+  if(req.body.username=="" ||  req.body.password==""){
+    req.flash('error', 'Please provide details.');
+    res.redirect('/auth/register');
+  }
   const registrationParams = req.body;
-  // const users = req.app.locals.users;
   const payload = {
     username: registrationParams.username,
     password: authUtils.hashPassword(registrationParams.password),
@@ -29,7 +32,7 @@ router.post('/register', (req, res, next) => {
  
   usermodel.checkAlreadyexists(req.app.locals.db,req.body.username).then(function(result) {
     if(result.length>0){      
-      req.flash('messages', 'User account already exists.');
+      req.flash('error', 'User account already exists.');
       res.redirect('/auth/register');
     }else{
       usermodel.insertUser(req.app.locals.db,payload).then(function(result) {
